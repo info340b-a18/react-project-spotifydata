d175cfd9d9b0c51c084dab331102b2bb3f233312
Ashish Israni <ashisrani@D-10-18-252-121.dhcp4.washington.edu>
2018-12-07 00:12:14 -0800

    firebase likes show up on user profile

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -5,27 +5,69 @@ import { ReactTabulator } from 'react-tabulator';
 import { Button, Table } from 'reactstrap';
 import {Bar} from 'react-chartjs-2';
 import FooterPage from './FooterPage';
+import firebase from 'firebase/app';
+import 'firebase/auth'; 
+import 'firebase/database';
 
 class UserProfile extends Component {
     
     constructor(props) {
         super(props);
+        this.showLikes = this.showLikes.bind(this);
+        this.state = {
+            userLikes: []
+        }
         this.userTableData = [];
+        this.showLikes();
+       
     }
 
 
     render() {
+        console.log(this.state.userLikes);
+        var likes = "";
         if (this.userTableData.length == 0) {
             var userTable = <h1 className="login">Please login with Spotify first.</h1>
         } else {
             var userTable =  <UserTable userTableData={this.userTableData}></UserTable>    
         }
+        if (this.state.userLikes.length != 0) {
+            likes = this.state.userLikes.map((like) => {
+                console.log(like);
+                return <LikeCard like={like}></LikeCard>
+            })
+        }
+        console.log(likes);
+        
         return(
             <div className="main-container">
                 {userTable}
+                {this.state.userLikes.length != 0 && 
+                <div>
+                    <h1>Your Likes:</h1>
+                    {likes}
+                </div>
+                }
                 <FooterPage />
+                
             </div>
         )
+    }
+
+    showLikes() {
+        let uid = localStorage.getItem('uid');
+        let task = "";
+        firebase.database().ref(uid).once('value').then(function(snapshot) {
+            snapshot = snapshot.val().likes; 
+            let taskKeys = Object.keys(snapshot);
+            let taskArr = taskKeys.map((key) => { //map array of keys into array of tasks
+                task = snapshot[key]; //access element at that key
+                return task; //save the key for later referencing!
+            })
+            return taskArr
+        }).then((taskArr) => {
+            this.setState({userLikes: taskArr})
+        });
     }
 
     async componentDidMount() {
@@ -45,8 +87,24 @@ class UserProfile extends Component {
                 this.setState({userTablePresent: true})
             });  
         }
+       
     }
 
+}
+
+class LikeCard extends Component {
+    render() {
+        console.log(this.props.like);
+        return(
+            <div className="col-sm">
+            <div className="card bg-dark text-white">
+                <div className="card-body">
+                  <h5 className="card-title">{this.props.like.name}{" by "}{this.props.like.artist}</h5>
+              </div>
+            </div>
+            </div>
+        )
+    }
 }
 
 class UserTable extends Component {
