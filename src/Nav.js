ecae3d578196d72409691ca610e5b789d4b4b5fb
Kyler Sakumoto <kylerws@uw.edu>
2018-12-06 20:23:46 -0800

    Changed navbar home

--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import logo from './img/spotify.png';
 
 class Nav extends Component {
     render() {
       return (
         <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-          <Link className="nav-link" to='/Home'>Spotify Stats</Link>
-          {/* <a className="navbar-brand" href="index.html">Spotify Stats</a> */}
+          <Link className="nav-link" to='/Home'>Home</Link>
           <div className="navbar-nav" id="navlinks">
               <ul className="navbar-nav">
                   <li className="nav-item">
-                    <Link className="nav-link" to='/Spotify/Songs'>Spotify Songs</Link>
+                    <Link className="nav-link" to='/Spotify/Songs'>Top Songs</Link>
                   </li>
                   <li className="nav-item">
-                    <Link className="nav-link" to='/Spotify/Artists'>Spotify Artists</Link>
+                    <Link className="nav-link" to='/Spotify/Artists'>Top Artists</Link>
                   </li>
                   <li className="nav-item">
                     <Link className="nav-link" to='/UserProfile'>User Profile</Link>
